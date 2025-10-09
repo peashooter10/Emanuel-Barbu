0882a38b1edb371f57108fcc34c797b3671befab
peashooter10 <97386784+peashooter10@users.noreply.github.com>
2025-10-09 19:13:03 +0300

    Add files via upload

---
+++ Lab2/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+using namespace std;
+
+struct Student {
+    string nume_complet, spec;
+    int an;
+    float media;
+};
+
+void sorteaza(Student *tab[], int n,bool (* comparator)(Student*, Student*)) {
+
+}
+
+int main() {
+
+    ifstream myFile("studenti.csv");
+    string nume_complet, spec, an, media;
+    if (myFile.is_open()) {
+        string line;
+        while (getline(myFile,line)) {
+            stringstream ss(line);
+
+            getline(ss,nume_complet,',');
+            cout<<nume_complet<<" ";
+
+            getline(ss,spec,',');
+            cout<<spec<<" ";
+
+            getline(ss,an,',');
+            cout<<an<<" ";
+
+            getline(ss,media,',');
+            cout<<media<<" ";
+
+            cout<<endl;
+
+        }
+    }myFile.close();
+
+    return 0;
+}
